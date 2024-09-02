3d6ff1d9b6ee6d89dcdf245ad814e5050bcd7f2f
hkxs <lalvarezf25@gmail.com>
2024-09-02 16:02:56 -0500

    Add dataclass for knobs

--- src/xmlcli_mod/xmlcli.py
+++ src/xmlcli_mod/xmlcli.py
@@ -24,6 +24,7 @@ import logging
 from xmlcli_mod import xmlclilib
 from xmlcli_mod.common.utils import is_root
 from xmlcli_mod.common.errors import RootError
+from xmlcli_mod.dataclasses.knobs import Knob
 
 
 log = logging.getLogger(__name__)
@@ -33,13 +34,41 @@ class XmlCli:
         if not is_root():
             raise RootError()
 
-        self.xml_knobs = None
+        self.xml_data = None
+        self._knobs = None
         xmlclilib.set_cli_access("Linux")
         xmlclilib.verify_xmlcli_support()
         self._get_xml_knobs()
 
     def _get_xml_knobs(self) -> None:
-          self.xml_knobs = xmlclilib.get_xml()
+          self.xml_data = xmlclilib.get_xml()
 
     def save_xml_knobs(self, filename: str) -> None:
-        self.xml_knobs.write(filename)
+        self.xml_data.write(filename)
+
+    @property
+    def bios_knobs(self):
+        if not self._knobs:
+            self._knobs = self._extract_knobs()
+        return self._knobs
+
+    def _extract_knobs(self):
+        knobs_dict = {}
+        bios_knobs = self.xml_data.getroot().find("biosknobs")
+        for knob in bios_knobs.findall("knob"):
+            knob_name = knob.attrib["name"]
+            knob_attributes = {
+                "name": knob_name,
+                "type": knob.attrib.get("type", ""),
+                "description": knob.attrib.get("description", ""),
+                "value": knob.attrib.get("description", ""),
+                "default": knob.attrib.get("description", ""),
+                "_size": knob.attrib["size"],
+                "_offset": knob.attrib["offset"],
+            }
+            if knob_attributes["type"] == "scalar":
+                knob_attributes["value"] = int(knob_attributes["value"])
+                knob_attributes["default"] = int(knob_attributes["default"])
+
+            knobs_dict[knob_name] = Knob(**knob_attributes)
+        return knobs_dict
