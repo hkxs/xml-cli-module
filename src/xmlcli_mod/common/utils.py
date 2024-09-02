2a42e8a06ee627f50f39a2237b4e0605904c5d69
hkxs <lalvarezf25@gmail.com>
2024-09-02 16:26:53 -0500

    Remove the "cleanup" thing

--- src/xmlcli_mod/common/utils.py
+++ src/xmlcli_mod/common/utils.py
@@ -21,7 +21,7 @@ from collections import OrderedDict
 
 # Custom imports
 
-from .configurations import XMLCLI_CONFIG, ENCODING, XMLCLI_DIR, OUT_DIR, PY3, STATUS_CODE_RECORD_FILE
+from .configurations import ENCODING, XMLCLI_DIR, STATUS_CODE_RECORD_FILE
 from defusedxml import ElementTree as ET
 
 log = logging.getLogger(__name__)
@@ -893,21 +893,6 @@ def load_nvar_xml(xml_file):
     db[key]["free_space"] = get_integer_value(db[key]["size"]) - occupied_size
     db[key]["next_offset"] = occupied_size
   return db
-
-
-def run_cleaner():
-  """Run cleaner to clean caches or out directory.
-
-  :return:
-  """
-  if XMLCLI_CONFIG.getboolean("INITIAL_CLEANUP", "CLEANUP"):
-    cleanup_extensions = XMLCLI_CONFIG.get("INITIAL_CLEANUP", "CACHE_CLEAN_EXTENSIONS").split(',')
-    try:
-      clean_cache(XMLCLI_DIR, extensions=cleanup_extensions)
-      if XMLCLI_CONFIG.getboolean("INITIAL_CLEANUP", "CLEAN_OUT_DIR"):
-        clean_directory(OUT_DIR)
-    except (PermissionError, WindowsError, OSError):
-      pass
 
 
 def store_buffer(dir_path, buffer, start, end, guid="", _type="FV", extract=False):
