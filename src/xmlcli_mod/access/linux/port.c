18bc1bd40fc77f2860e7020b2fdd92a0d5be3d90
hkxs <lalvarezf25@gmail.com>
2024-09-15 14:01:27 -0500

    Refactor C code for port

--- src/xmlcli_mod/access/linux/port.c
+++ src/xmlcli_mod/access/linux/port.c
@@ -6,37 +6,33 @@
 
 uint8_t *read_port(uint16_t port, uint8_t size)
 {
-  // Get access to all ports on the system
-  iopl(3);
+    // Get access to all ports on the system
+    iopl(3);
 
-  // Allocate our array to hold port reads
-  uint8_t *val = malloc(sizeof(uint8_t) * size);
+    // Allocate our array to hold port reads
+    uint8_t *val = malloc(sizeof(uint8_t) * size);
 
-  if (!val) {  // handle condition if malloc referenced to null pointer reference
-    free(val);
-    return 0;
-  } else {
-    // Read one byte from each port up to the size we want
-    int r;
-    for(r = 0; r < size; r++)
+    if (val)
     {
-      uint8_t byte = inb(port + r);
-      val[r] = byte;
+        for(int i = 0; i < size; i++)
+        {
+            uint8_t byte = inb(port + i);
+            val[i] = byte;
+        }
     }
 
     return val;
-  }
 }
 
 void write_port(uint16_t port, uint8_t size, uint32_t val)
 {
-  // Get access to all ports on the system
-  iopl(3);
+    uint8_t byteToWrite;
+    // Get access to all ports on the system
+    iopl(3);
 
-  int r;
-  for(r = 0; r < size; r++)
-  {
-    uint8_t byteToWrite = ((val >> (8 * r)) & 0xFF);
-    outb(byteToWrite, port + r);
-  }
+    for(int i = 0; i < size; i++)
+    {
+        byteToWrite = ((val >> (8 * i)) & 0xFF);
+        outb(byteToWrite, port + i);
+    }
 }
